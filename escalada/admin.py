913f140407b51a77908b5b20ee06039154f6c54c
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2022-05-26 00:32:33 -0500

    First models

--- escalada/admin.py
+++ escalada/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import User, ClassType, ClimbBooking
+
+class ClimbBookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'instructor', 'classType')
+
+class ClassTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type')
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(ClassType, ClassTypeAdmin)
+admin.site.register(ClimbBooking, ClimbBookingAdmin)
--- escalada/models.py
+++ escalada/models.py
@@ -1,7 +1,20 @@
-from re import A
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class ClassType(models.Model):
+    type = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.type
+    
+class ClimbBooking(models.Model):
+    classType = models.ForeignKey(ClassType, on_delete=models.SET_NULL, null=True, related_name= "class_type")
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="class_instructor")
+    date = models.DateTimeField()
+    
+class Post(models.Model):
+    text = models.CharField(max_length=500)
