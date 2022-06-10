717827d641e29d8498ca3f94c7e93c5bb3f18c4d
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2022-06-09 20:14:07 -0500

    Buy coupon functionality using JS

--- escalada/static/escalada/index.js
+++ escalada/static/escalada/index.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelectorAll('.enroll-link').forEach(link => {
         link.onclick = function(){
-            enrollButton('/enroll', link.dataset.climbclass)
+            buyButton('/enroll', '#enroll', '#buy-form', '#id_climbClass', link.dataset.climbclass, true)
+        }
+    })
+    document.querySelectorAll('.buy-coupon-link').forEach(link => {
+        link.onclick = function(){
+            buyButton('/buyCoupon', '#buy-coupon', '#buy-form', '#id_coupon', link.dataset.climbclass, false)
         }
     })
 })
 
-function enrollButton(url, classSelected){
+function buyButton(url, buyDiv, divToPlace, formSelect, classSelected, prePopulate){
     let request = new XMLHttpRequest();
     let method = 'GET';
     request.open(method, url);
@@ -14,10 +19,12 @@ function enrollButton(url, classSelected){
         // the response is the rendered HTML
         // which django sends as return render(response, "your_template.html", context)
         document.querySelector('#index').style.display = 'none';
-        var formDiv = document.querySelector('#enroll-buy-form');
+        var formDiv = document.querySelector(divToPlace);
         formDiv.innerHTML = request.response;
-        formDiv.innerHTML = document.querySelector('#enroll').innerHTML;
-        formDiv.querySelector('#id_climbClass').value = classSelected;
+        formDiv.innerHTML = document.querySelector(buyDiv).innerHTML;
+        if (prePopulate){
+            formDiv.querySelector(formSelect).value = classSelected;
+        }
         // This is the important part
         // Set that HTML to the new, templated HTML returned by the server
       };
